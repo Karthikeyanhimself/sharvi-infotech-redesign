dc1d8716872c7c13dbffa9157d1c5dd8fd091927
Karthikeyanhimself <karthikeyatiyyagura73@gmail.com>
2025-12-30 19:10:29 +0530

    seventh try of adding vercel analytics...

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
